808e075bff0ef8fca2742b44a3f1f93fc0304667
Goodmanc1998 <goodmanc1998@gmail.com>
2019-02-08 12:17:11 +0000

    Displayable Mesh
    Created a desplayable mesh

--- New Unity Project/Assets/MeshGenerator.cs
+++ New Unity Project/Assets/MeshGenerator.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 
 public class MeshGenerator : MonoBehaviour {
+
     Mesh mesh;
+
     List<Vector3> vertexList;
     List<int> triIndexList;
-
+    List<Vector2> UVList;
 
 	// Use this for initialization
 	void Start () {
@@ -19,26 +21,34 @@ public class MeshGenerator : MonoBehaviour {
 
         vertexList = new List<Vector3>();
         triIndexList = new List<int>();
+        UVList = new List<Vector2>();
 
-        CreateQuad();
+        CreateQuad(1,1);
 
         //Convert lists to arrays and store in mesh
         mesh.vertices = vertexList.ToArray();
         mesh.triangles = triIndexList.ToArray();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        //Convert UV list to array and store in mesh
+        mesh.uv = UVList.ToArray();
+
+        //Recalculating Normals to avoid Errors
+        mesh.RecalculateNormals();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
-    void CreateQuad()
+    void CreateQuad(int x, int y)
     {
         //List of Vertices
-        vertexList.Add(new Vector3(0, 1, 0));
-        vertexList.Add(new Vector3(1, 1, 0));
-        vertexList.Add(new Vector3(1, 0, 0));
-        vertexList.Add(new Vector3(0, 0, 0));
+        vertexList.Add(new Vector3(x, y + 1, 0));
+        vertexList.Add(new Vector3(x + 1, y + 1, 0));
+        vertexList.Add(new Vector3(x + 1, y, 0));
+        vertexList.Add(new Vector3(x, y, 0));
 
         //List of Triangle Indices
         triIndexList.Add(0);
@@ -47,6 +57,14 @@ public class MeshGenerator : MonoBehaviour {
         triIndexList.Add(1);
         triIndexList.Add(2);
         triIndexList.Add(3);
+
+        //Storing the grass texture
+        UVList.Add(new Vector2(0, 0.5f));
+        UVList.Add(new Vector2(0.5f, 0.5f));
+        UVList.Add(new Vector2(0.5f, 0));
+        UVList.Add(new Vector2(0, 0));
+
+
 
 
     }
